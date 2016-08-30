87d4ddfce388acf99a43d5c075e5844d0529b8a6
Richard Adem <richy486@gmail.com>
2016-08-30 09:50:13 -0400

    Initial Commit

---
+++ printer-server-io/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  printer-server-io
+//
+//  Created by Richard Adem on 8/30/16.
+//  Copyright © 2016 Richard Adem. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
