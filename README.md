d086c92b35e4ae5f53c263ca65bc7a5111f84d46
Richard Adem <richy486@gmail.com>
2017-01-06 16:49:42 -0500

    added postgres database

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 A basic vapor template for starting a new Vapor web application. If you're using vapor toolbox, you can use: `vapor new --template=basic`
 
+using tutorials:
+
+- https://github.com/vapor/example
+- http://vapor.university/?medium=article
+- https://www.twilio.com/blog/2016/10/getting-started-with-the-vapor-swift-web-framework.html
+- https://www.bignerdranch.com/blog/server-side-swift-with-vapor/
+
 ## 📖 Documentation
 
 Visit the Vapor web framework's [documentation](http://docs.vapor.codes) for instructions on how to use this package.
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,7 +1,15 @@
 import Vapor
 import HTTP
+import VaporPostgreSQL
 
 let drop = Droplet()
+drop.preparations.append(Friend.self)
+
+do {
+    try drop.addProvider(VaporPostgreSQL.Provider.self)
+} catch {
+    assertionFailure("Error adding provider: \(error)")
+}
 
 drop.group("v1") { v1 in
 
@@ -36,13 +44,43 @@ drop.group("v1") { v1 in
 
 drop.group("v2") { v2 in
     
+    /*
+     Get Postgres:
+     $ brew install postgres
+     
+     Setup pPostgres in terminal 1:
+     $ postgres -D /usr/local/var/postgres/
+     
+     Create Postgres database in terminal 2:
+     $ createdb friends
+     
+     View Postgress database in terminal 2:
+     $ psql friends
+     
+     Test post in terminal 3:
+     $ curl -H "Content-Type: application/json" -X POST -d '{"name": "Some Name","age": 30,"email": "email@email.com"}' http://localhost:8080/v2/friend
+     */
+    
     v2.get("friends") { req in
-        let friends = [Friend(name: "Sarah", age: 33, email:"sarah@email.com"),
-                       Friend(name: "Steve", age: 31, email:"steve@email.com"),
-                       Friend(name: "Drew", age: 35, email:"drew@email.com")]
-        let friendsNode = try friends.makeNode()
-        let nodeDictionary = ["friends": friendsNode]
-        return try JSON(node: nodeDictionary)
+        let friends = try Friend.all().makeNode()
+        let friendsDictionary = ["friends": friends]
+        return try JSON(node: friendsDictionary)
+    }
+    
+    v2.get("friends", Int.self) { req, userID in
+        guard let friend = try Friend.find(userID) else {
+            throw Abort.notFound
+        }
+        return try friend.makeJSON()
+    }
+    
+    v2.post("friend") { req in
+        print("req: \(req)")
+        print("req.json: \(req.json)")
+        
+        var friend = try Friend(node: req.json)
+        try friend.save()
+        return try friend.makeJSON()
     }
     
     v2.resource("posts", PostController())
