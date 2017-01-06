0d189941665c5c85f0e27fdc63decbb9b61c7e14
Richard Adem <richy486@gmail.com>
2017-01-06 12:47:41 -0500

    Added some simple routes with views and json

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,33 @@
 import Vapor
+import HTTP
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+drop.get("hello") { request in
+  let name = request.data["name"]?.string ?? "stranger"
+  return try drop.view.make("hello", [
+    "name": name
+  ])
 }
 
-drop.resource("posts", PostController())
+drop.post("person") { request in
+  guard let name = request.data["name"]?.string,
+        let city = request.data["city"]?.string else {
+    throw Abort.badRequest
+  }
+
+  return try Response(status: .created, json: JSON(node: [
+    "name": name,
+    "city": city
+  ]))
+}
+
+drop.get("something") { request in
+  let name = request.data["name"]?.string ?? "stranger"
+  return try Response(status: .created, json: JSON(node: [
+    "name": name,
+    "something": true
+  ]))
+}
 
 drop.run()
